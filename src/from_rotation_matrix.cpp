164302ce20918fc51b8bc704f6baa587be91410c
Christian Berg <christian_berg@live.no>
2020-03-07 18:23:20 +0100

    Improved numerical stability of fromRotatioMatrix methods

--- src/from_rotation_matrix.cpp
+++ src/from_rotation_matrix.cpp
@@ -4,10 +4,28 @@
 #include <orient/detail/so3_generator.hpp>
 #include <orient/detail/trigonometric_derivatives.hpp>
 
+template<class T>
+typename std::enable_if<not std::numeric_limits<T>::is_integer, bool>::type
+    almost_equal(T x, T y, int ulp)
+{
+    // the machine epsilon has to be scaled to the magnitude of the values used
+    // and multiplied by the desired precision in ULPs (units in the last place)
+    return std::fabs(x-y) <= std::numeric_limits<T>::epsilon() * std::fabs(x+y) * ulp
+        // unless the result is subnormal
+        || std::fabs(x-y) < std::numeric_limits<T>::min();
+}
+
 double trace(Eigen::Matrix3d const& R, Eigen::Ref<Eigen::Matrix<double, 1, 9>> H)
 {
   H << 1,0,0,0,1,0,0,0,1;
   return R.trace();
+}
+
+std::tuple<double, Eigen::Matrix<double, 1, 9>> traceWPD(Eigen::Matrix3d const& R)
+{
+  Eigen::Matrix<double, 1, 9> H;
+  H << 1,0,0,0,1,0,0,0,1;
+  return std::make_tuple(R.trace(), H);
 }
 
 Eigen::Vector3d errorVector(Eigen::Matrix3d const& R)
@@ -15,34 +33,47 @@ Eigen::Vector3d errorVector(Eigen::Matrix3d const& R)
   return orient::detail::vectorFromSkewSymmetric(R - R.transpose());
 }
 
-Eigen::Vector3d errorVector(Eigen::Matrix3d const& R, Eigen::Ref<Eigen::Matrix<double, 3, 9>> H)
+std::tuple<Eigen::Vector3d, Eigen::Matrix<double, 3, 9>>
+errorVectorWPD(Eigen::Matrix3d const& R)
 {
-  H = Eigen::Matrix<double, 3, 9>::Random();
+  Eigen::Matrix<double, 3, 9> H;
   H << orient::detail::generator<orient::Axis::x>, orient::detail::generator<orient::Axis::y>, orient::detail::generator<orient::Axis::z>;
-  return errorVector(R);
+  return std::make_tuple(errorVector(R), H);
 }
 
 namespace orient {
 
 Eigen::Vector3d angleAxisFromRotationMatrix(Eigen::Matrix3d const& R)
 {
-  const auto angle = std::acos((R.trace() - 1)/2);
-  return errorVector(R) * angle / (2*std::sin(angle));
+  // trace = 2*cos(angle) + 1
+  const auto trace = R.trace();
+  if( almost_equal(trace, 3.0, 10) ){
+    // angle is close to 2*k*pi
+    return orient::detail::vectorFromSkewSymmetric(R);
+  } else if( almost_equal(trace, -1.0, 10) ){
+    // angle is close to (2*k + 1)*pi
+    const Eigen::Matrix3d uut = (R + Eigen::Matrix3d::Identity()) / 2;
+    int max_i;
+    const auto ui = std::sqrt(uut.diagonal().maxCoeff(&max_i));
+    return M_PI * uut.block<3,1>(0, max_i) / ui;
+  } else {
+    const auto arg = (trace - 1)/2; 
+    const auto angle = arg + 1 > std::numeric_limits<double>::epsilon() ? std::acos(arg) : M_PI;
+    return errorVector(R) * angle / (2*std::sin(angle));
+  }
 }
 
 Eigen::Vector3d angleAxisFromRotationMatrix(
     Eigen::Matrix3d const& R,
     Eigen::Ref<Eigen::Matrix<double, 3, 9>> H)
 {
-  Eigen::Matrix<double, 1, 9> trHR;
-  const auto tr = trace(R, trHR);
-  const auto arg = (tr - 1)/2;
+  const auto[tr, trHR] = traceWPD(R);
+  const auto arg = 0.5*(tr - 1.0);
   Eigen::Matrix<double, 1 , 9> argHR = 0.5 * trHR;
   double angleHarg;
   auto angle = detail::acos(arg, angleHarg);
   Eigen::Matrix<double, 1, 9> angleHR = angleHarg * argHR;
-  Eigen::Matrix<double, 3, 9> evHR;
-  Eigen::Vector3d ev = errorVector(R, evHR);
+  const auto [ev, evHR] = errorVectorWPD(R);
   double k = angle / (2*std::sin(angle));
   double kHangle =  - 0.5 * (angle * std::cos(angle) / std::sin(angle) - 1) / std::sin(angle);
   H = ev * kHangle * angleHR + evHR * k;
@@ -51,22 +82,32 @@ Eigen::Vector3d angleAxisFromRotationMatrix(
 
 Eigen::Vector4d quaternionFromRotationMatrix(Eigen::Matrix3d const& R)
 {
-  const auto qw = std::sqrt(R.trace() + 1.0)/2.0;
-  const Eigen::Vector3d vec = errorVector(R) / (4*qw);
-  return (Eigen::Vector4d() << qw, vec).finished();
+  // tr(R) = 3*w^2 - x^2 - y^2 - z^2
+  //       = 4*w^2 - w^2 - x^2 - y^2 - z^2
+  //       = 4*w^2 - 1 
+  const auto tr = R.trace();
+  if( almost_equal(tr, -1.0, 10) ){
+    // w is close to 0
+    const Eigen::Matrix3d vvt = (R + Eigen::Matrix3d::Identity()) / 2;
+    int max_i;
+    const auto vi = std::sqrt(vvt.diagonal().maxCoeff(&max_i));
+    return (Eigen::Vector4d() << 0, vvt.block<3,1>(0, max_i) / vi).finished();
+  } else {
+    const auto qw = std::sqrt(tr + 1.0)/2.0;
+    const Eigen::Vector3d vec = errorVector(R) / (4*qw);
+    return (Eigen::Vector4d() << qw, vec).finished();
+  }
 }
 
 Eigen::Vector4d quaternionFromRotationMatrix(
     Eigen::Matrix3d const& R,
     Eigen::Ref<Eigen::Matrix<double, 4, 9>> H)
 {
-  Eigen::Matrix<double, 1, 9> trHR;
-  const auto tr = trace(R, trHR);
+  const auto [tr, trHR] = traceWPD(R);
   const auto qw = std::sqrt(tr + 1)/2;
   Eigen::Matrix<double, 1 , 9> qwHR = 0.25/std::sqrt(tr+1) * trHR;
 
-  Eigen::Matrix<double, 3, 9> evHR;
-  Eigen::Vector3d ev = errorVector(R, evHR);
+  const auto [ev, evHR] = errorVectorWPD(R);
   const Eigen::Vector3d vec = ev/(4*qw);
 
   H.block<1,9>(0,0) = qwHR;
--- test/from_rotation_matrix_test.cpp
+++ test/from_rotation_matrix_test.cpp
@@ -1,6 +1,7 @@
 #include <catch/catch2.hpp>
 
 #include <orient/from_rotation_matrix.hpp>
+#include <orient/normalize.hpp>
 
 #include <Eigen/Geometry>
 #include <gtsam/base/numericalDerivative.h>
@@ -9,80 +10,124 @@
 TEST_CASE("angleAxisFromRotationMatrix")
 {
   Eigen::Vector3d aa;
+  SECTION("Zero"){
+    aa = Eigen::Vector3d::Zero();
+  }
   SECTION("Random"){
     aa = Eigen::Vector3d::Random();
   }
   SECTION("half_PI_angle"){
     aa = Eigen::Vector3d::Random();
-    aa *= M_PI / (2*std::sqrt(aa.dot(aa)));
+    aa *= M_PI / (2*aa.norm());
   }
   SECTION("minus_half_PI_angle"){
     aa = Eigen::Vector3d::Random();
-    aa *= -M_PI / (2*std::sqrt(aa.dot(aa)));
+    aa *= -M_PI / (2*aa.norm());
   }
   SECTION("PI_angle"){
     aa = Eigen::Vector3d::Random();
-    aa *= M_PI / std::sqrt(aa.dot(aa));
+    aa *= M_PI / aa.norm();
   }
   SECTION("minus_PI_angle"){
     aa = Eigen::Vector3d::Random();
-    aa *= -M_PI / std::sqrt(aa.dot(aa));
+    aa *= -M_PI / aa.norm();
   }
   SECTION("2PI_angle"){
     aa = Eigen::Vector3d::Random();
-    aa *= 2*M_PI / std::sqrt(aa.dot(aa));
+    aa *= 2*M_PI / aa.norm();
   }
   SECTION("minus_2PI_angle"){
     aa = Eigen::Vector3d::Random();
-    aa *= -2*M_PI / std::sqrt(aa.dot(aa));
+    aa *= -2*M_PI / aa.norm();
+  }
+  SECTION("special1"){
+    aa << M_PI,0,0;
+  }
+  SECTION("special2"){
+    aa << 0,M_PI,0;
+  }
+  SECTION("special3"){
+    aa << 0,0,M_PI;
+  }
+  SECTION("special4"){
+    const auto a = M_PI/std::sqrt(2);
+    aa << a,a,0;
+  }
+  SECTION("special5"){
+    const auto a = M_PI/std::sqrt(2);
+    aa << a,0,a;
+  }
+  SECTION("special6"){
+    const auto a = M_PI/std::sqrt(2);
+    aa << 0,a,a;
   }
 
-  Eigen::Matrix3d R = gtsam::Rot3::Rodrigues(aa).matrix();
-  Eigen::AngleAxisd eaa{R};
-  Eigen::Vector3d expected = eaa.angle() * eaa.axis();
-  const Eigen::Vector3d actual = orient::angleAxisFromRotationMatrix(R);
-  std::cout << expected.transpose() << "\n";
-  std::cout << actual.transpose() << "\n";
+  const Eigen::Matrix3d R = gtsam::Rot3::Rodrigues(aa).matrix();
+  const Eigen::Vector3d actual_aa = orient::angleAxisFromRotationMatrix(R);
+  const Eigen::Matrix3d actual_R = gtsam::Rot3::Rodrigues(actual_aa).matrix();
+  CHECK( actual_R.isApprox(R) );
 }
 
 TEST_CASE("quaternionFromRotationMatrix")
 {
   Eigen::Vector3d aa;
+  SECTION("Zero"){
+    aa = Eigen::Vector3d::Zero();
+  }
   SECTION("Random"){
     aa = Eigen::Vector3d::Random();
   }
   SECTION("half_PI_angle"){
     aa = Eigen::Vector3d::Random();
-    aa *= M_PI / (2*std::sqrt(aa.dot(aa)));
+    aa *= M_PI / (2 * aa.norm());
   }
   SECTION("minus_half_PI_angle"){
     aa = Eigen::Vector3d::Random();
-    aa *= -M_PI / (2*std::sqrt(aa.dot(aa)));
+    aa *= -M_PI / (2*aa.dot(aa));
   }
   SECTION("PI_angle"){
     aa = Eigen::Vector3d::Random();
-    aa *= M_PI / std::sqrt(aa.dot(aa));
+    aa *= M_PI / aa.norm();
   }
   SECTION("minus_PI_angle"){
     aa = Eigen::Vector3d::Random();
-    aa *= -M_PI / std::sqrt(aa.dot(aa));
+    aa *= -M_PI / aa.norm();
   }
   SECTION("2PI_angle"){
     aa = Eigen::Vector3d::Random();
-    aa *= 2*M_PI / std::sqrt(aa.dot(aa));
+    aa *= 2*M_PI / aa.norm();
   }
   SECTION("minus_2PI_angle"){
     aa = Eigen::Vector3d::Random();
-    aa *= -2*M_PI / std::sqrt(aa.dot(aa));
+    aa *= -2*M_PI / aa.norm();
+  }
+  SECTION("special1"){
+    aa << M_PI,0,0;
+  }
+  SECTION("special2"){
+    aa << 0,M_PI,0;
+  }
+  SECTION("special3"){
+    aa << 0,0,M_PI;
+  }
+  SECTION("special4"){
+    const auto a = M_PI/std::sqrt(2);
+    aa << a,a,0;
+  }
+  SECTION("special5"){
+    const auto a = M_PI/std::sqrt(2);
+    aa << a,0,a;
+  }
+  SECTION("special6"){
+    const auto a = M_PI/std::sqrt(2);
+    aa << 0,a,a;
   }
 
   Eigen::Matrix3d R = gtsam::Rot3::Rodrigues(aa).matrix();
   Eigen::Quaterniond equat{R};
   Eigen::Vector4d expected = (Eigen::Vector4d() << equat.w(), equat.vec()).finished();
   const Eigen::Vector4d actual = orient::quaternionFromRotationMatrix(R);
-  std::cout << expected.transpose() << "\n";
-  std::cout << actual.transpose() << "\n";
-  std::cout << "-----\n";
+  CHECK( actual.isApprox( expected ) );
 }
 
 TEST_CASE("angleAxisFromRotationMatrix_derivative")
