86beb28ee0e6f2094bd3fcd0f56ecaf414ec70c4
Christian Berg <christian_berg@live.no>
2020-04-26 11:36:19 +0200

    Updated fromRotationMatrix to return nan matrix when jacobian not defined

--- src/from_rotation_matrix.cpp
+++ src/from_rotation_matrix.cpp
@@ -10,26 +10,20 @@ namespace orient {
 
 Eigen::Vector3d angleAxisFromRotationMatrix(Eigen::Matrix3d const& R)
 {
-  // trace = 2*cos(angle) + 1
-  const auto trace = R.trace();
-  if( detail::isAlmostEq(trace, 3.0) ){
+  const auto tr = R.trace();
+  if( detail::isAlmostEq(tr, 3.0) ){
     // angle is close to 2*k*pi
     // Should be minus I but the diagonal does not change anything
     return orient::detail::unskewSymmetric(R);
-  } else if( detail::isAlmostEq(trace, -1.0) ){
-    // Derivative is possibly not defined here. There are 2 solutions, as
-    // Theta and minus Theta would produce the same result ?
-    // soluation possible
-    // angle is close to (2*k + 1)*pi
+  } else if( detail::isAlmostEq(tr, -1.0) ){
     const Eigen::Matrix3d uut = (R + Eigen::Matrix3d::Identity()) / 2;
     int max_i;
     const auto ui = std::sqrt(uut.diagonal().maxCoeff(&max_i));
     return M_PI * uut.block<3,1>(0, max_i) / ui;
-  } else {
-    const auto arg = (trace - 1)/2; 
-    const auto angle = std::acos(arg);
-    return detail::unskewSymmetric(R - R.transpose()) * angle / (2*std::sin(angle));
   }
+  const auto arg = (tr - 1)/2; 
+  const auto angle = std::acos(arg);
+  return detail::unskewSymmetric(R - R.transpose()) * angle / (2*std::sin(angle));
 }
 
 std::pair<Eigen::Vector3d, Eigen::Matrix<double, 3, 9>> angleAxisFromRotationMatrixWD(Eigen::Matrix3d const& R)
@@ -38,7 +32,15 @@ std::pair<Eigen::Vector3d, Eigen::Matrix<double, 3, 9>> angleAxisFromRotationMat
   if( detail::isAlmostEq(tr, 3.0) ){
     const auto [ev, evJR] = detail::unskewSymmetricWPD(R);
     return std::make_pair(ev, evJR);
+  } else if( detail::isAlmostEq(tr, -1.0) ){
+    // Derivative is possibly not defined here. There are 2 solutions, as
+    // Theta and minus Theta would produce the same result ?
+    const Eigen::Matrix3d uut = (R + Eigen::Matrix3d::Identity()) / 2;
+    int max_i;
+    const auto ui = std::sqrt(uut.diagonal().maxCoeff(&max_i));
+    return std::make_pair(M_PI * uut.block<3,1>(0, max_i) / ui, Eigen::Matrix<double, 3, 9>::Constant(std::nan("")));
   }
+
   const auto x = 0.5*(tr - 1.0);
 
   const auto [angle, angleJx] = detail::acosWD(x);
@@ -80,6 +82,13 @@ Eigen::Vector4d quaternionFromRotationMatrix(Eigen::Matrix3d const& R)
 std::pair<Eigen::Vector4d, Eigen::Matrix<double, 4, 9>> quaternionFromRotationMatrixWD(Eigen::Matrix3d const& R)
 {
   const auto [tr, trJR] = detail::traceWD(R);
+  if( detail::isAlmostEq(tr, -1.0) ){
+    // w is close to 0
+    const Eigen::Matrix3d vvt = (R + Eigen::Matrix3d::Identity()) / 2;
+    int max_i;
+    const auto vi = std::sqrt(vvt.diagonal().maxCoeff(&max_i));
+    return std::make_pair((Eigen::Vector4d() << 0, vvt.block<3,1>(0, max_i) / vi).finished(),  Eigen::Matrix<double, 4, 9>::Constant(std::nan("")));
+  }
 
   const auto w = 0.5*std::sqrt(tr + 1.0);
   Eigen::Matrix<double, 1 , 9> wJR = 0.25/std::sqrt(tr+1) * trJR;
